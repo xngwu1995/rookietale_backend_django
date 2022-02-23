707bab3f8e2751930ab001cedd3411d28b2f5ea0
xngwu1995 <xngwu1995@gmail.com>
2022-02-22 21:45:46 -0600

    7-tweet-api-and-tests

--- Backend/Django/accounts/api/serializers.py
+++ Backend/Django/accounts/api/serializers.py
@@ -7,6 +7,11 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'email']
 
+class UserSerializerForTweet(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
---
+++ Backend/Django/tweets/api/views.py
@@ -0,0 +1,42 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from tweets.api.serializers import TweetSerializer, TweetSerializerForCreate
+from tweets.models import Tweet
+
+
+class TweetViewSet(viewsets.GenericViewSet):
+    # serializer_class = TweetCreateSerializer
+    serializer_class = TweetSerializerForCreate
+
+    def get_permissions(self):
+        if self.action == 'list':
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def list(self, request):
+        if 'user_id' not in request.query_params:
+            return Response('Missing user id', status=400)
+        user_id = request.query_params['user_id']
+        # SQL query is
+        # select * from twitter_tweets
+        # where user_id = xxx
+        # order by created_at desc
+        tweets = Tweet.objects.filter(user_id=user_id).order_by('-created_at')
+        serializer = TweetSerializer(tweets, many=True)
+        return Response({'tweets': serializer.data})
+
+    def create(self, request):
+        serializer = TweetSerializerForCreate(
+            data=request.data,
+            context={'request': request},
+        )
+        if not serializer.is_valid():
+            return Response({
+                "success": False,
+                "message": "Please check input",
+                "errors": serializer.errors,
+            }, status = 400)
+        # save will call create method in TweetSerializerForCreate
+        tweet = serializer.save()
+        return Response(TweetSerializer(tweet).data, status=201)
